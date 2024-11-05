ddc64b6c0ebdaba8cf5d5dc89de2cad5fff3bccf
tzapart <tomasz.t.zapart@gmail.com>
2024-11-05 11:51:33 +0100

    correct resnponsivness
    correct resnponsivness

--- Projekt1/Contacts/ContactSrcipt.js
+++ Projekt1/Contacts/ContactSrcipt.js
@@ -1,12 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.querySelector("form");
   const emailInput = document.getElementById("email");
+  const textArea = document.getElementById("wiadomosc");
 
   form.addEventListener("submit", function (event) {
     const emailValue = emailInput.value;
     if (!validateEmail(emailValue)) {
       alert("Proszę podać poprawny adres e-mail.");
       event.preventDefault();
+    } else if (!textArea) {
+      alert("Proszę podać wiadomość.");
+    } else {
+      alert("wiadomość wysłana");
     }
   });
 
